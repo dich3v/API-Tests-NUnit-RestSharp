4247df26df5b6d5cd882977fdbd81823c3893d33
dich3v <radoslav.dichev@abv.bg>
2025-05-22 12:57:50 +0300

    Add BlogApiTests with full CRUD coverage

--- ApiTestingProjects/EShopApi/EShopApi/BlogApiTests.cs
+++ ApiTestingProjects/EShopApi/EShopApi/BlogApiTests.cs
@@ -14,6 +14,7 @@ namespace EShopApi
     {
         private RestClient client;
         private string token;
+        private static string createdProductId;
 
         public void Dispose()
         {
@@ -54,6 +55,108 @@ namespace EShopApi
                 Assert.That(blog["author"]?.ToString(), Is.Not.Null.Or.Empty);
                 Assert.That(blog["category"]?.ToString(), Is.Not.Null.Or.Empty);
             }
+        }
+        [Test, Order(2)]
+
+        public void Test_AddBlog()
+        {
+            var request = new RestRequest("blog", Method.Post);
+            request.AddHeader("Authorization", $"Bearer {token}");
+            request.AddJsonBody(new
+            {
+                title = "New Blog Title",
+                description = "New Blog Description",
+                category = "New Blog Category",
+            });
+
+            var response = client.Execute(request);
+
+            Assert.Multiple(() =>
+            {
+
+                Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK), "Status code is not OK");
+
+                Assert.That(response.Content, Is.Not.Empty, "Response content is empty");
+
+                var content = JObject.Parse(response.Content);
+                createdProductId = content["_id"]?.ToString();
+
+                Assert.That(content["title"].ToString(), Is.EqualTo("New Blog Title"));
+                Assert.That(content["description"].ToString(), Is.EqualTo("New Blog Description"));
+                Assert.That(content["category"].ToString(), Is.EqualTo("New Blog Category"));
+                Assert.That(content["author"].ToString, Is.Null.Or.Not.Empty);
+            });
+        }
+        [Test, Order(3)]
+
+        public void Test_UpdateBlog()
+        {
+            var updateRequest = new RestRequest("blog/{id}", Method.Put);
+            updateRequest.AddUrlSegment("id", createdProductId);
+            updateRequest.AddHeader("Authorization", $"Bearer {token}");
+            updateRequest.AddJsonBody(new
+            {
+                title = "Updated Blog Title",
+                description = "Updated Blog Description",
+                category = "Updated Blog Category",
+            });
+
+            var updatedResponse = client.Execute(updateRequest);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(updatedResponse.StatusCode, Is.EqualTo(HttpStatusCode.OK), "Status code is not OK");
+
+                Assert.That(updatedResponse.Content, Is.Not.Empty, "Response content is empty");
+
+                var content = JObject.Parse(updatedResponse.Content);
+
+                Assert.That(content["title"].ToString(), Is.EqualTo("Updated Blog Title"));
+                Assert.That(content["description"].ToString(), Is.EqualTo("Updated Blog Description"));
+                Assert.That(content["category"].ToString(), Is.EqualTo("Updated Blog Category"));
+                Assert.That(content["author"].ToString, Is.Null.Or.Not.Empty);
+            });
+        }
+        [Test, Order(4)]
+        public void Test_GetBlogById()
+        {
+            var request = new RestRequest("blog/{id}", Method.Get);
+            request.AddUrlSegment("id", createdProductId);
+
+            var response = client.Execute(request);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK), "Status code is not OK");
+
+                Assert.That(response.Content, Is.Not.Empty, "Response content is empty");
+
+                var content = JObject.Parse(response.Content);
+
+                Assert.That(content["title"].ToString(), Is.EqualTo("Updated Blog Title"));
+                Assert.That(content["description"].ToString(), Is.EqualTo("Updated Blog Description"));
+                Assert.That(content["category"].ToString(), Is.EqualTo("Updated Blog Category"));
+            });
+        }
+        [Test, Order(5)]
+
+        public void Test_DeleteBlog()
+        {
+            var deleteRequest = new RestRequest("blog/{id}", Method.Delete);
+            deleteRequest.AddUrlSegment("id", createdProductId);
+            deleteRequest.AddHeader("Authorization", $"Bearer {token}");
+
+            var deleteResponse = client.Execute(deleteRequest);
+
+            Assert.That(deleteResponse.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+
+            var verifyGetRequest = new RestRequest("blog/{id}", Method.Get);
+
+            verifyGetRequest.AddUrlSegment("id", createdProductId);
+
+            var verifyResponse = client.Execute(verifyGetRequest);
+
+            Assert.That(verifyResponse.Content, Is.EqualTo("null"));
         }
     }
 }
